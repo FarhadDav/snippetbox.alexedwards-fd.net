a5d59a2d9fd7c2d6bf0d6065a8d48641439518f1
Farhad Davudov <farhad.davudov@raceroster.com>
2024-08-19 22:44:04 -0600

    Added more routes

--- main.go
+++ main.go
@@ -11,15 +11,38 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+// Add a snippetView handler function.
 func snippetView(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Displaying specific snippet"))
+}
+
+// Add a snippetCreate handler function.
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet"))
+}
+
+// Add a snippetCreate handler function.
+func snippetTest1(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("test1"))
+}
+
+// Add a snippetCreate handler function.
+func snippetTest2(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("test2"))
 }
 
 func main() {
 	// Use the http.NewServeMux() function to initialize a new servemux, then
 	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home) // Restrict this route to exact matches on / only.
+
+	// Register the two new handler functions and corresponding route patterns with
+	// the servemux, in exactly the same way that we did before.
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/snippet/test1/", snippetTest1)
+	mux.HandleFunc("/snippet/test1/test2", snippetTest2)
 
 	// Print a log message to say that the server is starting.
 	log.Print("starting server on :4000")
