b6adc5747bfec10160150dbf484c3a49ea14e477
Farhad Davudov <farhad.davudov@raceroster.com>
2024-09-03 19:48:04 -0600

    Template improvements

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -21,42 +21,42 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for _, snippet := range snippets {
-		fmt.Fprintf(w, "%+v\n", snippet)
+	data := templateData{
+		Snippets: snippets,
 	}
 
-	// // Initialize a slice containing the paths to the two files. It's important
-	// // to note that the file containing our base template must be the *first*
-	// // file in the slice.
-	// files := []string{
-	// 	"./ui/html/base.tmpl",
-	// 	"./ui/html/partials/nav.tmpl",
-	// 	"./ui/html/pages/home.tmpl",
-	// }
+	// Initialize a slice containing the paths to the two files. It's important
+	// to note that the file containing our base template must be the *first*
+	// file in the slice.
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
 
-	// // Use the template.ParseFiles() function to read the template file into a
-	// // template set. If there's an error, we log the detailed error message, use
-	// // the http.Error() function to send an Internal Server Error response to the
-	// // user, and then return from the handler so no subsequent code is executed.
-	// ts, err := template.ParseFiles(files...)
-	// if err != nil {
-	// 	// Because the home handler is now a method against the application
-	// 	// struct it can access its fields, including the structured logger. We'll
-	// 	// use this to create a log entry at Error level containing the error
-	// 	// message, also including the request method and URI as attributes to
-	// 	// assist with debugging.
-	// 	app.serverError(w, r, err)
-	// 	return
-	// }
+	// Use the template.ParseFiles() function to read the template file into a
+	// template set. If there's an error, we log the detailed error message, use
+	// the http.Error() function to send an Internal Server Error response to the
+	// user, and then return from the handler so no subsequent code is executed.
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		// Because the home handler is now a method against the application
+		// struct it can access its fields, including the structured logger. We'll
+		// use this to create a log entry at Error level containing the error
+		// message, also including the request method and URI as attributes to
+		// assist with debugging.
+		app.serverError(w, r, err)
+		return
+	}
 
-	// // Then we use the Execute() method on the template set to write the
-	// // template content as the response body. The last parameter to Execute()
-	// // represents any dynamic data that we want to pass in, which for now we'll
-	// // leave as nil.
-	// err = ts.ExecuteTemplate(w, "base", nil)
-	// if err != nil {
-	// 	app.serverError(w, r, err)
-	// }
+	// Then we use the Execute() method on the template set to write the
+	// template content as the response body. The last parameter to Execute()
+	// represents any dynamic data that we want to pass in, which for now we'll
+	// leave as nil.
+	err = ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, r, err)
+	}
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
